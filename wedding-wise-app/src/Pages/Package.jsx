5ff445679efa0842f13fb6caddb434b4c2768ced
Omri Cohen <omri.cohen1010@gmail.com>
2024-05-11 22:21:59 +0300

    Update Package.jsx

--- wedding-wise-app/src/Pages/Package.jsx
+++ wedding-wise-app/src/Pages/Package.jsx
@@ -4,11 +4,9 @@ import UserWithoutPackage from "../components/UserWithoutPackage";
 import UserPackage from "../components/UserPackage";
 import { Box } from "@mui/material";
 import { AppContext } from "../store/AppContext";
-function Package() {
-  const [userStatus, setUserStatus] = useState(null);
-  // console.log(JSON.parse(sessionStorage.getItem("currentUser")).package)
-  const { userData } = useContext(AppContext)
 
+function Package() {
+  const { userData } = useContext(AppContext);
   return (
     <Box sx={{ display: "block", width: "100%" }}>
       {userData === null ? (
