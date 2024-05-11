ebf3c72c1c471c8664097dd03e0fd7c5913a385a
Tom keynan <tomk133@gmail.com>
2024-05-11 18:40:09 +0300

    tasks and finance
    new tasks and finance pages

---
+++ wedding-wise-app/src/Pages/Tasks.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import TaskListComp from "../components/TasksPage/TaskListComp";
+
+/*import { Checkbox, Button, LinearProgress, IconButton, List, ListItem, ListItemText, ListItemSecondaryAction } from '@mui/material';
+import { ArrowDropDown } from '@mui/icons-material';
+*/
+
+function Tasks() {
+  return (
+    <div>
+      <TaskListComp />
+    </div>
+  );
+}
+
+export default Tasks;
