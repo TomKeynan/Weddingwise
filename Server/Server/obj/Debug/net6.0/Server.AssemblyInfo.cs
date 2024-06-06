21ad20750c2f86852e75eaba8a2db3f943c79704
DeadlyAcorn <adamv350@gmail.com>
2024-06-06 22:07:48 +0300

    version 2.0 - major changes
     dbservices overhaul has been done.
    - couple details update added.
    - insertion, retrieving , and updating of a supplier has been added.
    - getting the top ranked suppliers of each type added.
    
    note: before every implementation at the client's side, there is a  need to contact lord lamm.

--- Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e8a51faf2fd306e8b09febc8fb05ff7b85fbfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71651923b919deb08b2bb34bd9de12b2099f3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
